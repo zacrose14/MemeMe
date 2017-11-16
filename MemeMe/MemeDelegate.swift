013cf8e9b562b9c4d50c4610fb8d39a84d121f13
Zac Rose <zac@insightmobiledevelopment.com>
2017-11-16 14:03:35 -0500

    Image picker is working and correctly picking photos from user library. Added code to dismiss the picker after user selects a photo.
    Next:
    Add AutoLayout to make image fill screen correctly
    Add text fields to allow user to make meme.
    Add camera option so that users to can take photos for memes.

---
+++ MemeMe/MemeDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  MemeDelegate.swift
+//  MemeMe
+//
+//  Created by Zachary Rose on 11/16/17.
+//  Copyright © 2017 Zachary Rose. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeDelegate: NSObject, UIImagePickerControllerDelegate {
+    
+    
+    
+}
--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    // MARK: Outlets
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    // MARK: Variables
+    var info: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Action from button
+    @IBAction func pickImage(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    
     }
-
-
+    
+    // Function to pick image and load to screen.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
 }
-
